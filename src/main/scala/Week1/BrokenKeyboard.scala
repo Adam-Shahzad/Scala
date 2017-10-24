93e15c239f5062cda391301fdcc981b73e85bf26
adam-shahzad <adam.shahzad@outlook.com>
2017-10-24 15:27:55 +0000

    Just finished chat bot

---
+++ src/main/scala/Week1/BrokenKeyboard.scala
@@ -0,0 +1,18 @@
+package Week1
+import scala.io.Source
+object BrokenKeyboard extends App
+{
+  val inputs:List[String] = List("edcf","qwer","hjklo")
+  main(3,inputs )
+
+  def main(repeated : Int, inputs :List[String]): Unit ={
+    for( a <-inputs){checkLongest(a)}}
+
+  def checkLongest(input:String) = {
+    var temp = ""
+    val listOfLines = Source.fromFile("C:\\Users\\Administrator\\IdeaProjects\\ScalaThings\\src\\main\\scala\\Week1\\data.txt").getLines.toList
+    listOfLines.foreach(x => {if (input.sorted.contains(x.distinct.sorted) && x.length > temp.length) { temp = x}})
+    println(s"${input} = $temp")
+  }
+
+}
